3e946a2022b3040abb2fa731122027f2801296b1
ChewyChocolate <116084618+ChewyChocolate@users.noreply.github.com>
2022-10-18 21:00:29 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double pi = 3.14;
+    double radius, rdsSqrd, area;
+
+    cout << "Enter the diameter: " << endl;
+    cin >> radius;
+
+    area = rdsSqrd * rdsSqrd * pi;
+
+
+    cout << "The Area is " << area;
+
+    return 0;
+}
